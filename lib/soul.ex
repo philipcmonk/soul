97193db25f94c3235ccf0c0b955fe5c59bceeeab
Philip Monk <phil@pcmonk.me>
2016-10-27 01:30:49 -0700

    github api testing

--- lib/soul.ex
+++ lib/soul.ex
@@ -1,5 +1,21 @@
+require Logger
+
 defmodule Soul do
   def add(x, y) do
     x + y + y
+  end
+
+  def getGithubUsername() do
+    IO.puts "go to this url:  " <> Github.authorize_url!
+    code = String.strip(IO.gets "code: ")
+    client = Github.get_token!(code: code)
+    case OAuth2.Client.get(client, "/user") do
+      {:ok, %OAuth2.Response{status_code: 401, body: _}} ->
+        Logger.error("Unauthorized token")
+      {:ok, %OAuth2.Response{status_code: status_code, body: user}} ->
+        user["login"]
+      {:error, %OAuth2.Error{reason: reason}} ->
+        Logger.error("Error: #{inspect reason}")
+    end
   end
 end
