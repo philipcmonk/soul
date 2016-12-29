a4c9a059ae79ae249657b0758c76d71e10c4d5fc
Philip Monk <phil@pcmonk.me>
2016-12-29 16:42:04 -0700

    fixed foursquare query options

--- apps/soul_gut/lib/sources/foursquare.ex
+++ apps/soul_gut/lib/sources/foursquare.ex
@@ -7,9 +7,9 @@ defmodule Sources.Foursquare do
 
   def get_endpoint(endpoint) do
     client = Fs.client
-    case Client.get(client, "/" <> Enum.join(endpoint, "/")
-                    <> "?oauth_token=" <> client.token.access_token
-                    <> "&v=20161229&m=swarm") do
+    case Client.get(client, "/" <> Enum.join(endpoint, "/"), [],
+                    params: [oauth_token: client.token.access_token,
+                             v: "20161229", m: "swarm"]) do
       {:ok, %OAuth2.Response{status_code: _status_code, body: body}} ->
         body
       {:error, %OAuth2.Error{reason: reason}} ->
